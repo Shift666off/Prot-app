067dd7ca8457d361ec739aa414f1ede82a7dc1f0
Shift666off <shift666@proton.me>
2026-04-15 14:53:40 +0500

    Add Socks5SettingsPageView with DataContext initialization

--- src/Client/ProtonVPN.Client/UI/Main/Settings/Pages/Connection/Socks5SettingsPageView.xaml.cs
+++ src/Client/ProtonVPN.Client/UI/Main/Settings/Pages/Connection/Socks5SettingsPageView.xaml.cs
@@ -1,1 +1,13 @@
+using Microsoft.UI.Xaml.Controls;
 
+namespace ProtonVPN.Client.UI.Main.Settings.Pages.Connection
+{
+    public sealed partial class Socks5SettingsPageView : UserControl
+    {
+        public Socks5SettingsPageView()
+        {
+            this.InitializeComponent();
+            this.DataContext = new Socks5SettingsPageViewModel();
+        }
+    }
+}
