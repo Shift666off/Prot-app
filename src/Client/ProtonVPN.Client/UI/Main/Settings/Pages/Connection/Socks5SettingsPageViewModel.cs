487496aa8c5570b0ba68b1031a7c4f409be3fdec
Shift666off <shift666@proton.me>
2026-04-15 14:54:39 +0500

    Add Socks5SettingsPageViewModel for proxy settings

--- src/Client/ProtonVPN.Client/UI/Main/Settings/Pages/Connection/Socks5SettingsPageViewModel.cs
+++ src/Client/ProtonVPN.Client/UI/Main/Settings/Pages/Connection/Socks5SettingsPageViewModel.cs
@@ -1,1 +1,43 @@
 
+using ProtonVPN.Client.Common;
+
+namespace ProtonVPN.Client.UI.Main.Settings.Pages.Connection
+{
+    public class Socks5SettingsPageViewModel : ViewModelBase
+    {
+        private bool _socksProxyEnabled;
+        public bool SocksProxyEnabled
+        {
+            get => _socksProxyEnabled;
+            set => SetProperty(ref _socksProxyEnabled, value);
+        }
+
+        private string _socksProxyHost;
+        public string SocksProxyHost
+        {
+            get => _socksProxyHost;
+            set => SetProperty(ref _socksProxyHost, value);
+        }
+
+        private string _socksProxyPort;
+        public string SocksProxyPort
+        {
+            get => _socksProxyPort;
+            set => SetProperty(ref _socksProxyPort, value);
+        }
+
+        private string _socksProxyUsername;
+        public string SocksProxyUsername
+        {
+            get => _socksProxyUsername;
+            set => SetProperty(ref _socksProxyUsername, value);
+        }
+
+        private string _socksProxyPassword;
+        public string SocksProxyPassword
+        {
+            get => _socksProxyPassword;
+            set => SetProperty(ref _socksProxyPassword, value);
+        }
+    }
+}
